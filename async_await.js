2f1add9a75f2f9fadc207dc097ab7278d4561d80
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-04-03 12:38:36 +0530

    Update async_await.js

--- async_await.js
+++ async_await.js
@@ -34,6 +34,30 @@ let new_data = { name: "tanushree", location: "kolkata" };
 
 Guvi(new_data, Suman);
 
+// FETCH WITH ASYNC / AWAIT
+
+const url = "https://623d55e87efb5abea68ce31d.mockapi.io/suman/friends/";
+
+async function Suman(url) {
+  let data = await fetch(url);
+  if (data.ok) {
+    return data.json();
+  } else {
+    return data.status;
+  }
+}
+
+Suman(url)
+  .then(function (value) {
+    console.log(JSON.stringify(value));
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    console.log("fetch() worked successfully !");
+  });
+
 
 // SIMPLE EXAMPLE
 
