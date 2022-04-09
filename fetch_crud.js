a1eaf5948729d9dc0489b5b86894d9ca77416cdd
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-04-09 11:42:14 +0530

    Create fetch_crud.js

---
+++ fetch_crud.js
@@ -0,0 +1,33 @@
+// CRUD operations using Fetch API()
+
+"use strict";
+
+const url = "https://62511ff6977373573f448b61.mockapi.io/suman/friends";
+
+// FETCH Data from the MockAPI Server
+async function Suman(url) {
+  let data = await fetch(url);
+  if (data.ok) {
+    return data.json();
+  } else {
+    return data.status;
+  }
+}
+
+let suman_object = Suman(url);
+
+suman_object
+  .then(function (value) {
+    console.log(JSON.stringify(value));
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    console.log("fetch api ran successfully !");
+  });
+  
+// FETCH API in a new way
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(JSON.stringify(data)));
