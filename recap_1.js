bafc4b611846169c8b51ed151ed7605879eb20e5
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-04-10 12:21:52 +0530

    Update recap_1.js

--- recap_1.js
+++ recap_1.js
@@ -12,4 +12,34 @@ function Guvi(data) {
 
 setTimeout(Suman, 5000, "Suman Gangopadhyay", Guvi);
 
+// promise & promise chaining
+
+function Guvi(data) {
+  for (let i = 1; i <= 10; i++) {
+    let result = data * i;
+    console.log(result);
+  }
+}
+
+let suman_promise = new Promise(function (resolve, reject) {
+  let number = 2;
+  if (number % 2 == 0) {
+    resolve(Guvi(number));
+  } else {
+    reject("odd number # rejected");
+  }
+});
+
+// console.log(suman_promise);
+
+// call a promise object
+
+suman_promise
+  .then(function (value) {
+    console.log(value);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
 
